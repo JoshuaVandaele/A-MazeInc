534bb4ea6a8a9ef6ca471507acb26ef1d1a26144
Joshua Vandaële <joshua@vandaele.software>
2018-10-30 19:03:34 +0100

    V1.0

---
+++ sprites.lua
@@ -0,0 +1,40 @@
+--===============================
+--==       CUSTOMIZATION       ==
+--===============================
+
+local character = "@" -- Must be only one character
+local wall = "#" -- Must be only one character
+local goal = "O" -- Must be only one character
+local spikes = "^" -- Must be only one character
+local teleporter = "0" -- Must be only one character
+local invisible_wall = "W" -- Must be only one character
+local ground = " " -- Must be only one character
+local winning_message = "\n  __   __            _    _               _ \n  \\ \\ / /           | |  | |             |"..spikes.."|\n   \\ V /___  _   _  | |  | | ___  _ __   |"..wall.."|\n    \\ // _ \\| | | | | |/\\| |/ _ \\| '_ \\  |"..goal.."|\n    | | (_) | |_| | \\  /\\  / (_) | | | | |"..teleporter.."|\n    \\_/\\___/ \\__,_|  \\/  \\/ \\___/|_| |_| ("..character..")" -- as many crap as you want
+local loose_message = [[                                                                     
+ _|      _|                        _|                        _|      
+   _|  _|    _|_|    _|    _|      _|    _|_|      _|_|_|  _|_|_|_|  
+     _|    _|    _|  _|    _|      _|  _|    _|  _|_|        _|      
+     _|    _|    _|  _|    _|      _|  _|    _|      _|_|    _|      
+     _|      _|_|      _|_|_|      _|    _|_|    _|_|_|        _|_|]] -- as many crap as you want
+
+
+local character = character:sub(1,1)
+local wall = wall:sub(1,1)
+local goal = goal:sub(1,1)
+local spikes = spikes:sub(1,1)
+local teleporter = teleporter:sub(1,1)
+local invisible_wall = invisible_wall:sub(1,1)
+
+return {
+["character"] = character, 
+["wall"] = wall, 
+["goal"] = goal, 
+["spikes"] = spikes,
+["win_msg"] = winning_message,
+["loose_msg"] = loose_message, 
+["teleport"] = teleporter, 
+["inv_wall"] = invisible_wall,
+["ground"] = ground, 
+}
+
+--Ascii art credits: http://patorjk.com/software/taag/
